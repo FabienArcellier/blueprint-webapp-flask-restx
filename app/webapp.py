ae766fbcfbc635d8985963a8e42947f7ab32a2f6
Fabien Arcellier <fabien.arcellier@gmail.com>
2021-02-11 09:37:46 +0100

    initial commit

---
+++ app/webapp.py
@@ -0,0 +1,17 @@
+# pylint: disable=invalid-name
+
+from flask import Flask
+from flask_restx import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'fabien'}
+
+
+if __name__ == "__main__":
+    app.run()
